a95a3eb89987376f74863ca98930cdad1fd64588
max <maxlundh91@protonmail.com>
2022-01-01 15:49:25 +0100

    added clicky analytics

--- Pages/Me.razor.cs
+++ Pages/Me.razor.cs
@@ -5,6 +5,6 @@ public partial class Me
     private string? profilePicSource = "images/maxlundh.jpeg";
     private string? githubLink = "https://github.com/cryptojanne";
     private string? twitterLink = "https://twitter.com/cryptojanne";
-    private string? emailAddress = "maxlundh91@protonmail.com";
+    private string? emailAddress = "mailto:maxlundh91@protonmail.com";
 
 }
