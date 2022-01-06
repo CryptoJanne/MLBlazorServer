7a70747f94a756c1fa24d1cf81df09900276723d
max <maxlundh91@protonmail.com>
2022-01-06 18:15:16 +0100

    pivoted the article idea

--- ArticleEngine/ArticlePreview.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace mlblazorserver.ArticleEngine;
-using Microsoft.AspNetCore.Components;
-
-public class ArticlePreviewComponent : ComponentBase
-{
-    [Parameter] public Article articlePost { get; set; }
-}
--- Pages/Articles.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace mlblazorserver.Pages;
-using mlblazorserver.ArticleEngine;
-
-public partial class Articles
-{
-    List<Article> asdf = new ArticleController()._articles;
-}
